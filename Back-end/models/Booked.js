b23909d53ca67836fd8422f4b730e3bfb1fd4aeb
Jahangir Ahamd <jsofi502@gmail.com>
2022-04-18 11:03:26 +0530

    Fix: Typos

--- Back-end/models/Booked.js
+++ Back-end/models/Booked.js
@@ -1,16 +1,16 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const BookingSchema = new mongoose.Schema(
   {
-    userId: { type: String },  
+    userId: { type: String },
     ProductType: { type: String },
     quantity: { type: Number, default: 1 },
     No_of_prople: { type: Number },
-    price: { type: Number},
+    price: { type: Number },
     address: { type: Object },
-    status: { type: String, default: "pending" },      
+    status: { type: String, default: 'pending' },
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("Booking", BookingSchema);
+module.exports = mongoose.model('Booking', BookingSchema);
