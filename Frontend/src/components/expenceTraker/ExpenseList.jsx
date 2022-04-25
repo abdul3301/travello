02be4d17a4a7c46317a2208b502a20ac4635f8e8
saikumar-27 <saiyadav27398@gmail.com>
2022-04-25 12:42:28 +0530

    iternary styling

--- Frontend/src/components/expenceTraker/ExpenseList.jsx
+++ Frontend/src/components/expenceTraker/ExpenseList.jsx
@@ -1,42 +1,43 @@
-import React, { useContext, useState, useEffect } from 'react';
-import ExpenseItem from './ExpenseItem';
-import { AppContext } from '../../context/AppContext';
-import "./expensetraker.css"
+import React, { useContext, useState, useEffect } from "react";
+import ExpenseItem from "./ExpenseItem";
+import { AppContext } from "../../context/AppContext";
+import "./expensetraker.css";
 
 const ExpenseList = () => {
-	const { expenses } = useContext(AppContext);
+  const { expenses } = useContext(AppContext);
 
-	const [filteredExpenses, setfilteredExpenses] = useState(expenses || []);
+  const [filteredExpenses, setfilteredExpenses] = useState(expenses || []);
 
-	useEffect(() => {
-		setfilteredExpenses(expenses);
-	}, [expenses]);
+  useEffect(() => {
+    setfilteredExpenses(expenses);
+  }, [expenses]);
 
-	const handleChange = (event) => {
-		const searchResults = expenses.filter((filteredExpense) =>
-			filteredExpense.name.toLowerCase().includes(event.target.value)
-		);
-		setfilteredExpenses(searchResults);
-	};
+  const handleChange = (event) => {
+    const searchResults = expenses.filter((filteredExpense) =>
+      filteredExpense.name.toLowerCase().includes(event.target.value),
+    );
+    setfilteredExpenses(searchResults);
+  };
 
-	return (
-		<>
-			<input className='searchbar'
-				type='text'
-				placeholder='Type to search...'
-				onChange={handleChange}
-			/>
-			<ul >
-				{filteredExpenses.map((expense) => (
-					<ExpenseItem 
-						id={expense.id}
-						name={expense.name}
-						cost={expense.cost}
-					/>
-				))}
-			</ul>
-		</>
-	);
+  return (
+    <>
+      <input
+        className="searchbar"
+        type="text"
+        placeholder="  Type to search..."
+        onChange={handleChange}
+      />
+      <ul>
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            id={expense.id}
+            name={expense.name}
+            cost={expense.cost}
+          />
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default ExpenseList;
