dda232b96d7dba72c272b03b49032c9ad8a18367
abdul3301 <abdulrahimsilvery@gmail.com>
2022-05-14 17:25:39 +0530

    deleted src

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://api.unsplash.com",
-    headers:{
-        Authorization: "Client-ID OX3Z5DVC4hHn_L8hqSDXrX7E6dNcfX6LZaF1uOlrlXI"
-    }
-})
